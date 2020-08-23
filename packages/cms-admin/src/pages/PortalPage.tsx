6a3c3b9f252e2f5550d1ac4c7509852ba2816d91
mutoe <mutoe@foxmail.com>
2020-08-23 12:37:11 +0800

    fix(assets): remove useless icon reference
    affects: cms-admin

--- packages/cms-admin/src/pages/PortalPage.tsx
+++ packages/cms-admin/src/pages/PortalPage.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
 import useAuthorizationContext from 'src/contexts/authorization.context'
-import logo from 'src/logo.svg'
 
 const PortalPage: React.FC = () => {
   const { profile, unmountAuthorization } = useAuthorizationContext()
 
   return <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p> {'Mutoe\'s Content Manage System'} </p>
 
       {profile
